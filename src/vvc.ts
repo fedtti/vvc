aa8aeedd10fef4103d7db254ab67e56b63ef8189
Federico Pinna <fpinna@vivocha.com>
2018-06-14 16:56:10 +0200

    Updated to support the new commands

--- src/vvc.ts
+++ src/vvc.ts
@@ -6,7 +6,9 @@ import { meta } from './lib/config';
 program
   .version(meta.version)
   .description(meta.description)
+  .command('info', 'Print info on the currently logged in user')
   .command('login', 'Login to you Vivocha account')
+  .command('logout', 'Logout from your Vivocha account')
   .command('widget', 'Manage engagement widgets')
   .command('strings', 'Manage translation strings')
   .parse(process.argv);
