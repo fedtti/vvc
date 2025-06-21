705ff7dd84cafd72f19ee036c9a5403561703581
Federico Moretti <hello@federicomoretti.it>
2025-06-21 12:08:38 +0200

    fix: update ws() to use fetch() instead of request() in 'lib/ws.ts'

--- src/lib/ws.ts
+++ src/lib/ws.ts
@@ -20,27 +20,39 @@ export const wsUrl = async (path: string): Promise<string> => {
          Promise.reject(new Error('Configuration file not found: perform a login to create it.'));
 };
 
+/**
+ * Performs a WebSocket request to the specified path with the given options and expected status codes.
+ * @param {string} path - The path to the WebSocket API endpoint.
+ * @param {any} options - Optional configuration options for the request, such as headers or query parameters.
+ * @param {number[]} okStatusCodes - An array of expected HTTP status codes that indicate a successful response.
+ *                                   Defaults to [200, 201].
+ * @returns {Promise<any>} - A promise that resolves with the response data if the request is successful.
+ * @throws {RequestError} - If the request fails or the response status is not in the expected status codes. 
+ */
 export const ws = async (path: string, options?: any, okStatusCodes: number[] = [ 200, 201 ]): Promise<any> => {
   const config = await readConfig();
   const url = await wsUrl(path);
-  // return new Promise((resolve, reject) => {
-  //   request(Object.assign({ // TODO: Replace with fetch().
-  //     method: 'GET',
-  //     url,
-  //     auth: {
-  //       user: config.userId,
-  //       pass: config.secret,
-  //       sendImmediately: true
-  //     },
-  //     json: true
-  //   }, opts), function(err, resp, body) {
-  //     if (err || okStatusCodes.indexOf(resp.statusCode) === -1) {
-  //       reject(new RequestError(err, resp, body));
-  //     } else {
-  //       resolve(body);
-  //     }
-  //   });
-  // });
+  return new Promise(async (resolve, reject) => {
+    try {
+      const response = await fetch(url, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Basic ${Buffer.from(`${config.username}:${config.password}`).toString('base64')}`,
+        },
+        ...options
+      });
+      if (!response.ok || okStatusCodes.indexOf(response.status) === -1) {
+        const body = await response.text();
+        reject(new RequestError(`Request failed with status: ${response.status}.`, null, response, body));
+      } else {
+        const data = await response.json();
+        resolve(data);
+      }
+    } catch (error) { 
+      reject(new RequestError(`Request failed: ${error.message}.`, error, null, null));
+    }
+  });
 };
 
 export const retriever = async (url: string): Promise<void> => {
